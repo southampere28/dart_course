e6c55705fc76338e3b4e029c17c84b32789fcb1a
Pramudya <pramudyap333@gmail.com>
2024-06-27 23:39:09 +0700

    new update

---
+++ bin/19switch.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  String mark = 'E';
+
+  switch (mark) {
+    case 'A':
+      print('you are passed with great mark!');
+      break;
+    case 'B':
+      print('good grade');
+      break;
+    case 'C':
+      print('you are passed');
+      break;
+    case 'D':
+      print('you are fail');
+      break;
+    default:
+      print('wrong input');
+  }
+}
