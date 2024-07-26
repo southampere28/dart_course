c2a4764b1bc258df3fc7fd4c8dc706048269542f
Pramudya <pramudyap333@gmail.com>
2024-07-26 22:07:00 +0700

    new update

--- bin/dart oop/crs11inheritance.dart
+++ bin/dart oop/crs11inheritance.dart
@@ -2,11 +2,25 @@ class Manager {
   String? name;
 
   void sayHello(String name) {
-    print('Hello $name, my name is ${this.name}');
+    print('Hello $name, my name is Manager ${this.name}');
   }
 }
 
-class VicePresident extends Manager {}
+class VicePresident extends Manager {
+  // Overriding method from parent class
+  @override
+  void sayHello(String name) {
+    print('Hello $name, my name is VP ${this.name}');
+  }
+}
+
+class CLevel extends Manager {
+  // Overriding method from parent class
+  @override
+  void sayHello(String name) {
+    print('Hello $name, my name is CLevel ${this.name}');
+  }
+}
 
 void main(List<String> args) {
   var manager = Manager();
@@ -17,5 +31,7 @@ void main(List<String> args) {
   vp.name = "Nopal";
   vp.sayHello('Andreas');
 
-  
+  var clevel = CLevel();
+  clevel.name = "Farhan";
+  clevel.sayHello('Claudya');
 }
